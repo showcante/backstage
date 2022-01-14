729f4c5f999e6ecf1686de783636deebb68d836c
Dennis Ricardo Cavalcante <dennis.cavalcante@inmetrics.com.br>
2022-01-14 11:10:18 -0300

    adicionando skeleton terraform

---
+++ Skeleton/main.tf
@@ -0,0 +1,82 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_vpc" "vpc_teste" {
+    cidr_block = var.vpc_cidr_block
+    tags = {
+        Name: "${var.env_prefix}-vpc"
+    }
+}
+
+resource "aws_subnet" "Public_Subnet" {
+    vpc_id = aws_vpc.vpc_teste.id
+    cidr_block = var.subnet_cidr_block
+    availability_zone = var.avail_zone
+    tags = {
+        Name: "${var.env_prefix}-Public-subnet"
+    }
+}
+
+resource "aws_internet_gateway" "cliente-igw" {
+    vpc_id = aws_vpc.vpc_teste.id
+    tags = {
+        Name: "${var.env_prefix}-igw"
+    }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+    default_route_table_id = aws_vpc.vpc_teste.default_route_table_id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.cliente-igw.id
+    }
+    tags = {
+        Name: "${var.env_prefix}-main-rtb"
+    }
+}
+
+
+resource "aws_security_group" "allow_ssh" {
+    name = "Acesso SSH"
+    description = "Permitir acesso SSH na maquina"
+    vpc_id = aws_vpc.vpc_externo.id
+
+    ingress  {
+        
+        description      = "SSH para maquina de teste"
+        from_port        = 21
+        to_port          = 21
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+       
+    }
+
+
+    egress  {
+        
+        from_port        = 0
+        to_port          = 0
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    
+
+    tags = {
+        Name = "Permitir SSH"
+    }
+}
+
+resource "aws_instance" "linux_teste" {
+    ami = var.image_name
+    instance_type = var.instance_type
+    subnet_id = aws_subnet.Public_Subnet.id
+    availability_zone = var.avail_zone
+    key_name = "Backstage_test"
+    vpc_security_group_ids = [aws_security_group.allow_rdp.id]
+    tags  = {
+        Name = "${var.env_prefix}-backstage"
+    }
+}
---
+++ Skeleton/terraform.tfvars
@@ -0,0 +1,6 @@
+vpc_cidr_block = "10.0.0.0/16"
+subnet_cidr_block = "10.0.1.0/24"
+avail_zone = "us-east-1a"
+env_prefix = "teste"
+instance_type = "t2.micro"
+image_name = "ami-08e4e35cccc6189f4"
